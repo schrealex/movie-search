04d1c0c9e21fcd9ded96d47e921d52df23e3ab12
schrealex <cer34ll@gmail.com>
2020-02-17 13:08:09 +0000

    Now able to toggle adult searches

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import {library} from '@fortawesome/fontawesome-svg-core';
+import {faCoffee, faHeat} from '@fortawesome/pro-solid-svg-icons';
 import {faFilm} from '@fortawesome/pro-light-svg-icons';
 import {faPopcorn} from '@fortawesome/pro-duotone-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
 import dayjs from 'dayjs';
 
-library.add(faFilm, faPopcorn);
+library.add(faCoffee, faFilm, faHeat, faPopcorn);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
