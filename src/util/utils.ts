89576e28f6124df836718ffc96ad96fa9a08891b
Ceryl Bruitzman <ceryl.bruitzman@topicus.nl>
2023-12-14 23:17:18 +0100

    Request images over HTTPS

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,6 +1,6 @@
 export default {
     getImageUrl: (size: number, filePath: string): string => {
         if (!filePath) return '';
-        return 'http://image.tmdb.org/t/p/' + size + filePath;
+        return 'https://image.tmdb.org/t/p/' + size + filePath;
     },
 };
